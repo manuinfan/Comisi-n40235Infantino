92d876dfa3122e6c1a7a3e94a25eb1e40e325d3c
manuinfan <manu.infantino1@gmail.com>
2022-08-25 21:28:40 -0300

    CicloFor

---
+++ app.js
@@ -0,0 +1,39 @@
+// aqui voy a mencionar las variables 
+/*
+let nombreAlumno = "Manuel" // string
+console.log(nombreAlumno)
+let edad = 32 // numbrer
+console.log(edad)
+let estudiante = true // booleam
+console.log(estudiante)
+
+const apellido = "Infantino"
+console.log(apellido)
+
+// diferencias entre LET y CONST
+*/
+/*
+// pueden inicializarse vacias
+// Redecladaras
+let nombre;
+nombre = "Coder"
+nombre = "house"
+nombre = 37
+console.log(nombre)
+*/
+/*ss
+// deben ser declaradas con valor
+// no pueden ser redeclaradas
+const NOMBRE = "Coder"
+*/
+/*
+let nombre = "Manuel"
+let apellido = "Infantino"
+
+
+let numero1 = Number(prompt("Ingrese el primer numero"))
+let numero2 = Number(prompt("Ingrese el segundo numero"))
+
+let resultado = numero1 + numero2
+
+alert(resultado)*/
---
+++ app2.js
@@ -0,0 +1,5 @@
+var numeroDeIngresos = 65
+
+for( i = 1; i <= numeroDeIngresos; i++ ){
+    document.write( i + "<br>");
+}
